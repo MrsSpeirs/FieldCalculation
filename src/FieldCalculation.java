e5f4938a7413efe622a6c5823181a2acf0906e6a
MrsSpeirs <sspeirs@dasd.org>
2020-09-12 11:33:16 -0400

    Change in the comments for program description

---
+++ src/FieldCalculation.java
@@ -0,0 +1,14 @@
+/*sspeirs
+ * 9/12/2020, 11:26 AM */
+
+public class FieldCalculation {
+    public static void main(String args[]) {
+        /*You are to write a program that calculates, using
+          * math operators, how many centimeters
+          * are within three football fields that measure 120 yards
+          * each.  Note that 30.48 centimeters are in 1 foot.
+         */
+
+
+    }
+}
